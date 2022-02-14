8b1f2bb269dafb810926529d2f2856416995142f
AL@BWI <liew.audi+github@gmail.com>
2022-02-14 15:09:55 +0800

    Add viewmodel

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.cupcake.model
 
-class OrderViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel : ViewModel() {
+    private var _orderQuantity = MutableLiveData<Int>(0)
+    val orderQuantity: LiveData<Int> = _orderQuantity
+
+    private val _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
+
+    fun setQuantity(numberCupcakes: Int) {
+        _orderQuantity.value = numberCupcakes
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+    }
 }
